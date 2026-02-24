7c5e45f28d4bb028d4c5ff1d4785cdd08f7fad9b
omar mohamed <omarabualmagdo6@gmail.com>
2026-02-25 01:36:27 +0200

    user.js

--- backend/middleware/authMiddleware.js
+++ backend/middleware/authMiddleware.js
@@ -1,5 +1,5 @@
 const jwt = require('jsonwebtoken');
-const User = require('../models/User');
+const User = require('../models/user');
 
 // Protect routes - verify JWT token
 exports.protect = async (req, res, next) => {
